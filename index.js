09fd438d1f6434e05a2c5c03c10881de3e1fcd4e
Matthias Müller <MattDiMu@users.noreply.github.com>
2015-04-01 21:41:41 +0200

    first working version

--- index.js
+++ index.js
@@ -1,78 +1,72 @@
 //require what we need
-var through = require('through2');
-var gutil = require('gulp-util');
+var through = require('through2')
+, gutil = require('gulp-util')
+, fs = require('fs');
 
-var DirectoryColorfy = require('directory-colorfy');
-var DirectoryEncoder = require('directory-encoder');
+const PLUGIN_NAME = 'gulp-sassvg:';
 
-const PLUGIN_NAME = 'gulp-prefixer';
+const DATA_PREFIX = "data:image/svg+xml;charset=US-ASCII,";
+
+function encodeSvg(content){
+    return DATA_PREFIX + encodeURIComponent( content.toString('utf-8')
+                .replace(/[\t\n\r]/gmi, " ") //replace tab, linefeed and carrriage return
+                .replace(/<\!\-\-(.*(?=\-\->))\-\->/gmi, "") //remove comments
+            )
+            .replace(/\(/g, "%28") // opening brackets
+            .replace(/\)/g, "%29") // closing brackets
+            .replace(/"/g, "%22"); // double quotes 
+}
+
+function cleanSvg(content){
+    return content.toString().replace(/'/gm, "\"");   
+}
 
 
+function decodeSassVariables(content){
+    return content.replace(/%24fillcolor/gm, "#{$fillcolor}")
+            .replace(/%24strokecolor/gm, "#{$strokecolor}")
+            .replace(/%24extrastyles/gm, "#{$extrastyles}   ");
+}
+
+function addSassVariables(content){
+    content = content.replace(/(fill=")((?!none).*)(")/gmi, "$1$fillcolor$3") //add $fillcolor
+        .replace(/(style *= *"[^"]*)(fill *: *)([^;"]*)/gmi, "$1$2$fillcolor") //add $fillcolor to styles
+        .replace(/(stroke=")((?!none).*)(")/gmi, "$1$strokecolor$3") //add $strokecolor
+        .replace(/(style *= *"[^"]*)(stroke *: *)([^;"]*)/gmi, "$1$2$strokecolor"); //add $strokecolor to styles
+    if(content.match(/(<svg[^>]*style="[^>"]*)(")([^>]*>)/) != null){
+        return content.replace(/(<svg[^>]*style=")([^>"]*")([^>]*>)/gmi, "$1$extrastyles$2$3");
+    }else{
+        return content.replace(/(<svg[^>]*)(>)/gmi, '$1 style="$extrastyles"$2');
+    }
+}
+                           
 var gulpSassvg = function(options){
+    this.options = options || {};
+    this.options.tmpDir = this.options.tmpDir || "./.tmp-sassvg/"; //TODO add some options
+    this.options.outputFile = this.options.outputFile || "./scss/_icons.scss"; //TODO add some options
     
-    return through.obj(function(file, enc, cb){
-        if(file.isNull){
-            cb();
-        }
-        
-            cb();
+    var template = fs.readFileSync(__dirname + "/mixin.template", "utf8").split("#####REPLACED#####");
+    this.header = template[0];
+    this.footer = template[1];
     
+    var writeStream = fs.createWriteStream(this.options.outputFile);
+    writeStream.write(header);
     
+    function listStream(file, enc, cb){
+        var filePath = file.path;
+        var fileName = filePath.split('\\').pop().split('/').pop().replace(/\.[^/.]+$/, ""); 
+        writeStream.write("\n\t'" + fileName + "': '" + decodeSassVariables(encodeSvg(addSassVariables(cleanSvg(file.contents)))) + "',");
+        cb();
+    }
     
-    });
-
+    function endStream(cb){
+        writeStream.write(footer);
+        writeStream.end();
+        cb();
+    }
+    return through.obj(listStream, endStream);
+}
 module.exports = gulpSassvg;
 
 
-// TODO
-if (ERROR-TO-BE-DEFINED){
- throw new gutil.PluginError(PLUGIN_NAME, 'ERROR-MESSSAGE-TO-BE-DEFINED');   
-}
-
-
-
-
-
-
-// through2 is a thin wrapper around node transform streams
-var gutil = require('gulp-util');
-var PluginError = gutil.PluginError;
-
-// Consts
-
-function prefixStream(prefixText) {
-  var stream = through();
-  stream.write(prefixText);
-  return stream;
-}
-
-// Plugin level function(dealing with files)
-function gulpPrefixer(prefixText) {
-
-  if (!prefixText) {
-    throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
-  }
-  prefixText = new Buffer(prefixText); // allocate ahead of time
-
-  // Creating a stream through which each file will pass
-  return through.obj(function(file, enc, cb) {
-    if (file.isNull()) {
-      // return empty file
-      cb(null, file);
-    }
-    if (file.isBuffer()) {
-      file.contents = Buffer.concat([prefixText, file.contents]);
-    }
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(prefixStream(prefixText));
-    }
-
-    cb(null, file);
-
-  });
-
-};
-
-// Exporting the plugin main function
-module.exports = gulpPrefixer;
 
