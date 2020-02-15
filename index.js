076ca3e5977fde852ffe4bfdaadf2321e0592715
Sarah Ferguson <sarah.an.ferguson@gmail.com>
2020-02-15 17:23:30 -0500

    mixin option
    boolean option called 'outputMixins' which generated a _sassvg-mixins.scss file in order to preserve the original colors of multicolored mixins

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function folderNameFromPath(filePath){
     if(firstSplit.length > 1){
         return firstSplit[firstSplit.length - 2];
     }
-    
+
     var secondSplit = filePath.split('/');
     if(secondSplit.length > 1){
         return secondSplit[secondSplit.length - 2];
@@ -37,13 +37,11 @@ function sassVarRegex(variableName, optionalSuffix){
     return new RegExp(encodeURIComponent("#{$") + variableName + encodeURIComponent("}") + suffix, "gm"); // #{$variableName}
 }
 
-
-
 function addVariables(filePath, fileContent){
         var $ = cheerio.load(fileContent, {
             normalizeWhitespace: true,
             xmlMode: true
-        });
+		});
 		if($('svg').length !== 1){
 			throw new PluginError(PLUGIN_NAME, "Wrong SVG-File at '" + filePath +  "'.");
 		}
@@ -55,27 +53,26 @@ function addVariables(filePath, fileContent){
 		$('[style]').each(function(){
 			var fillValue = $(this).css("fill");
 			if(fillValue !== undefined && fillValue !== 'none'){
-				$(this).css("fill", "#{$fillcolor}");	
+				$(this).css("fill", "#{$fillcolor}");
 			}
 			var strokeValue = $(this).css("stroke");
 			if(strokeValue !== undefined && strokeValue !== 'none'){
-				$(this).css("stroke", "#{$strokecolor}");	
+				$(this).css("stroke", "#{$strokecolor}");
 			}
 		});
 		$('svg').each(function(){
 			var styles = $(this).attr("style");
 			$(this).css("empty", "empty;#{$extrastyles}"); //not the very best solution, but makes it valid and works everytime - empty props will be regexed out again
 		});
-        $('[stroke]').not('[stroke=none]').attr('stroke', '#{$strokecolor}');
-        return $.html('svg'); //return only the svg    
+		$('[stroke]').not('[stroke=none]').attr('stroke', '#{$strokecolor}');
+        return $.html('svg'); //return only the svg
 }
 
 function encodeSVG(dynamicContent){
     return encodeURIComponent(dynamicContent.replace(/[\t\n\r]/gmi, " ")) //replace tab, linefeed and carriage return
         .replace(/\(/g, "%28") // opening brackets
         .replace(/\)/g, "%29") // closing brackets
-        .replace(/["']/g, "%22"); // double quotes 
-  
+		.replace(/["']/g, "%22"); // double quotes
 }
 
 function decodeVariables(encodedContent){
@@ -89,29 +86,38 @@ function assembleDataString(fileName, finalContent){
     return "@function sassvg-" + fileName + "($fillcolor, $strokecolor, $extrastyles){ @return '" + DATA_PREFIX + finalContent + "'; }\n";
 }
 
-function optimizeSvg(writeStream, cb, filePath, svgString){
+function optimizeSvg(writeStream, writeMixinStream, cb, filePath, svgString){
     svgo.optimize(svgString).then(function(result) {
 		var optimizedSvg;
 		if(result.error){
 			throw new PluginError(PLUGIN_NAME, "SVG couldn't be optimized: '" + file.path +  "', will try to SASSVG it without optimizing.");
 			optimizedSvg = String(file.contents);
 		}else{
-			optimizedSvg = result.data;	
+			optimizedSvg = result.data;
 		}
-		sassvgIt(writeStream, cb, filePath, svgString);
+		sassvgIt(writeStream, writeMixinStream, cb, filePath, svgString);
     });
 }
 
-function sassvgIt(writeStream, cb, filePath, svgString){
+let originalSVG = {
+	scssFunc : (fileName, svgString) => {
+		let svgCode = encodeSVG(svgString);
+		return `@mixin sassvg-origin-${fileName} { background-image: url('${DATA_PREFIX}${svgCode}'); background-repeat: no-repeat; }\n`;
+	}
+}
+
+function sassvgIt(writeStream, writeMixinStream, cb, filePath, svgString){
+	if ( writeMixinStream ) {
+		writeMixinStream.write(
+			originalSVG.scssFunc(fileNameFromPath(filePath), svgString)
+		);
+	}
 	writeStream.write(
 		assembleDataString(
 			fileNameFromPath(filePath),
 			decodeVariables(
 				encodeSVG(
-					addVariables(
-						filePath,
-						svgString
-					)
+					addVariables(filePath, svgString)
 				)
 			)
 		)
@@ -119,38 +125,47 @@ function sassvgIt(writeStream, cb, filePath, svgString){
 	cb();
 }
 
-var options                           
+var options
 var gulpSassvg = function(optionsGiven){
-    options = optionsGiven || {};
+	options = optionsGiven || {};
     options.tmpDir = optionsGiven.tmpDir || "./.tmp-sassvg/"; //TODO still necessary?
-    options.outputFolder = optionsGiven.outputFolder || "./scss/"; //TODO add some options
+	options.outputFolder = optionsGiven.outputFolder || "./scss/"; //TODO add some options
+	options.outputMixins = optionsGiven.outputMixins || false;
+
   if( options.outputFolder.indexOf("/", options.outputFolder.length - 1) === -1 ){
     options.outputFolder = options.outputFolder + "/";
   }
 		options.outputMainFile = options.outputFolder + "_sassvg.scss";
+		options.outputMixinFile = options.outputFolder + "_sassvg-mixins.scss";
 		options.outputDataFile = options.outputFolder + "_sassvg-data.scss";
 		options.optimizeSvg = (optionsGiven.optimizeSvg !== undefined) ? optionsGiven.optimizeSvg : true; //true = 25% less filesize, but 3 times as long to create the sass file
-	
+
 		var writeStreamMain = fs.createWriteStream(options.outputMainFile);
 		writeStreamMain.write(fs.readFileSync(__dirname + "/_sassvg.scss", "utf8"));
 		writeStreamMain.end();
-	
-    var writeStream = fs.createWriteStream(options.outputDataFile);
+
+	var writeStream = fs.createWriteStream(options.outputDataFile);
+	let writeMixinStream;
+	if ( options.outputMixins === true ) {
+		writeMixinStream = fs.createWriteStream(options.outputMixinFile)
+	} else {
+		writeMixinStream = false;
+	}
 
     var sassvgMap = "\n\n$sassvg-map: (";
-	
+
     function listStream(file, enc, cb){
 		var folderName = folderNameFromPath(file.path);
 		var fileName = fileNameFromPath(file.path);
 		sassvgMap += "'" + fileName + "': ('name': '" + fileName + "', 'folder': '" + folderName + "'),";
 		if(options.optimizeSvg){
-			optimizeSvg(writeStream, cb, file.path, String(file.contents))
+			optimizeSvg(writeStream, writeMixinStream, cb, file.path, String(file.contents))
 		}else{
-			sassvgIt(writeStream, cb, file.path, String(file.contents));
+			sassvgIt(writeStream, writeMixinStream, cb, file.path, String(file.contents));
 		}
 
     }
-    
+
     function endStream(cb){
 				sassvgMap += ");";
 				writeStream.write(sassvgMap);
@@ -160,5 +175,3 @@ var gulpSassvg = function(optionsGiven){
     return through.obj(listStream, endStream);
 }
 module.exports = gulpSassvg;
-
-
